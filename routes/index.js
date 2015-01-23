4321d3a770f7c35d28211b7ca4080764b28182d3
blakeparkinson <blake@edmodo.com>
2015-01-23 00:40:19 -0800

    initial mongo

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var http = require("http"),
+    mongojs = require("mongojs"),
+    uri = 'mongodb://root:root@ds031541.mongolab.com:31541/rosterblitz';
 
-var mysql =  require('mysql');
+    db = mongojs.connect(uri, ["teams"]);
 
-var connection =  mysql.createConnection({
-  	host : "127.0.0.1",
-  	user : "root",
-  	password: ""
-  });
 
-/*connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
-});
-
-connection.end();
-*/
 var request = require('request');
 var nba_key = 'hdgj9e9vs9hquzc6ds22wtdy';
 var nfl_key = 'b4cwkbyqfyq25fcruevj5hw2';
@@ -28,10 +16,21 @@ var nba_key = 'hdgj9e9vs9hquzc6ds22wtdy',
 	teams = [];
 
 
+ db.open(function(err,db){
+      db.collection('teams',function(err,collection){
+        collection.find().toArray(function(err, nba_teams) {
+          console.log(nba_teams);
+        })
+      }) //collection
+    }); //open
+  
 request('https://api.sportsdatallc.org/nba-t3/league/hierarchy.json?api_key=' + nba_key, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     var json_response = (JSON.parse(response.body));
     var teams = formatTeams(json_response);
+    mongoInsert(teams);
+
+
     
  	teams.sort(compare);
     // don't render the page until we have formatted our teams
@@ -54,13 +53,27 @@ function compare(a,b) {
   return 0;
 }
 
+//inserts the record into mongo. 
+//Plan is to fetch the team id from mongo and only 
+//hit api as fallback and then update mongo
+function mongoInsert(teams){
+  db.open(function(err, client){
+    client.collection("teams", function(err, col) {
+      for (var i = 0; i < teams.length; i++) {
+        col.insert({id:teams[i].id, team:teams[i].name, market:teams[i].market}, function() {});
+      }
+    })
+  });
+
+}
+
 
 var formatTeams = function(hierarchy_response){
   for (i=0;i<hierarchy_response.conferences.length;i++){
 		for (j=0;j<hierarchy_response.conferences[i].divisions.length;j++){
 			for(k=0; k< hierarchy_response.conferences[i].divisions[j].teams.length; k++){
         		teams.push(hierarchy_response.conferences[i].divisions[j].teams[k]);
-      		}
+      }
 		}   
 	}
 return teams
