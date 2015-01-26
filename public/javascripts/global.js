4b7276f8df778899c6027116659e63356d222caf
Kelsey <kelseyjgross@gmail.com>
2015-01-25 22:20:51 -0800

    add player array

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -18,7 +18,8 @@ function fetchTeam(event) {
      	url: 'https://api.sportsdatallc.org/nba-t3/seasontd/2014/reg/teams/'+team+'/statistics.json?api_key=hdgj9e9vs9hquzc6ds22wtdy',
      		success: function(response){
      		 	details = formatRoster(response);
-                console.log(details);
+          detailsP = formatPlayers(response);
+                console.log(detailsP);
                 return details;
      		}
     });
@@ -50,8 +51,31 @@ function compare(a,b) {
 }
 
 
-
-
+var formatPlayers = function(response){
+  playersarray = [];
+  for (i=0;i<response.players.length;i++){
+    playersarray[i] = {};
+    for(var key in response.players[i]){
+      if (key != "total" && key != "average"){
+        var value = response.players[i][key];
+        playersarray[i][key] = value;
+      }
+      else if (key =="total"){
+        for(var key in response.players[i].total) {
+          var value = response.players[i].total[key];
+          playersarray[i]["total_"+key] = value;
+        }
+      }
+      else {
+        for(var key in response.players[i].average) {
+          var value = response.players[i].average[key];
+          playersarray[i]["average_"+key] = value;
+        }
+      }
+    }
+  }
+  return playersarray;
+}
 
 
 
