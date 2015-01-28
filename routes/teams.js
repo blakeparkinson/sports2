13802559e645aca45a2f71e9ce8ccae2e2b208e2
Kelsey <kelseyjgross@gmail.com>
2015-01-28 08:25:16 -0800

    format players documnets

--- routes/teams.js
+++ routes/teams.js
@@ -5,6 +5,7 @@ var http = require("http"),
     uri = 'mongodb://root:root@ds031541.mongolab.com:31541/rosterblitz';
 
     db = mongojs.connect(uri, ["teams"]);
+    db_players = mongojs.connect(uri, ["players"]);
 
 
 
@@ -24,20 +25,100 @@ router.get('/team', function(req, res) {
 });
 
 
-// when players endpoint is hit, call the api using that team_id
+// when players endpoint is hit, call the API/DB using that team_id
 router.get('/players', function(req, res) {
   temp = req.query.split('=');
   team_id = temp[1];
   console.log("team id is: "+team_id);
-  request('https://api.sportsdatallc.org/nba-t3/seasontd/2014/reg/teams/'+team_id+'/statistics.json?api_key='+nba_key, function (error, response, body) {
-
-    if (!error && response.statusCode == 200) {
-          res.json(response);
-    } 
-    else{
-      console.log("ruh roh spaghetti oh");
-    }
-  })
+  fetchPlayers(team_id)
 });
+
+
+
+var fetchPlayers = function(team_id){
+  var mongoresult = db_players.players.find( { id: team_id } );  // Or however you query Mongo
+  var now = new Date();
+  var target = new Date(mongoresult.last_updated);
+  var daterange = now.getTime() - target.getTime();
+  // if no results or the results are more than 24 hours old...
+  if (mongoresult.length == 0 || daterange > 86400){
+    var players = fetchPlayersFromApi(team_id);
+    var teamDocument = formatPlayersDocument(team_id, players);
+    mongoInsertPlayers(teamDocument);
+    console.log(teamDocument);
+    return teamDocument;  //pass this over to client side to render
+  }
+  else { 
+    return mongoresult //already fresh in the DB so just pass this over to client side to render
+  }
+  
+}
+
+var fetchPlayersFromApi = function(team_id){
+  request('https://api.sportsdatallc.org/nba-t3/seasontd/2014/reg/teams/'+team_id+'/statistics.json?api_key='+nba_key)
+    var json_response = (JSON.parse(response.body));
+    players = formatPlayers(json_response);
+    return players
+}
+
+
+function mongoInsertPlayers(players){
+  db_players.open(function(err, client){
+    client.collection("players", function(err, col) {
+     db_players.players.insert( players );
+    })
+  });
+
+}
+
+var formatPlayersDocument = function(team_id, players){
+  teamDocument = {};
+  teamDocument[team_id] = team_id;
+  teamDocument[id] = db.teams.find( { team_id: team_id }, { id: 1);
+  teamDocument[team_name] = db.teams.find( { team_id: team_id }, { name: 1);
+  teamDocument[market] = db.teams.find( { team_id: team_id }, { market: 1);
+  teamDocument[players] = players;
+  return teamDocument;
+}
+
+
+var formatPlayers = function(response){
+  playersarray = [];
+  for (i=0;i<response.players.length;i++){
+    playersarray[i] = {};
+    for(var key in response.players[i]){
+      if (key != "total" && key != "average"){
+        var value = response.players[i][key];
+        playersarray[i][key] = value;
+      }
+      else if (key =="total"){
+        for(var key in response.players[i].total) {
+          var value = response.players[i].total[key];
+          playersarray[i]["total_"+key] = value;
+        }
+      }
+      else {
+        for(var key in response.players[i].average) {
+          var value = response.players[i].average[key];
+          playersarray[i]["average_"+key] = value;
+        }
+      }
+    }
+  }
+  return playersarray;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
