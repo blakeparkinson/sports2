0a0327c0d4ab4a4b32fc442737140f2a4ac0e04b
blakeparkinson <blake@edmodo.com>
2015-01-29 00:42:42 -0800

    Adding the most boring sport in the universe

--- scripts/team_import.js
+++ scripts/team_import.js
@@ -4,6 +4,7 @@
 	
 var express = require('express');
 var router = express.Router();
+var parseString = require('xml2js').parseString;
 var http = require("http"),
     mongojs = require("mongojs"),
 
@@ -22,7 +23,7 @@ var api_key = '';
 var ver = '';
 var endpoint = '';
 var teams = [];
-var supported_leagues = ['nba', 'nfl'];
+var supported_leagues = ['nba', 'nfl', 'mlb'];
 
 //process.argv grabs the command line arguments
 var league = process.argv[2];
@@ -47,15 +48,14 @@ switch (league){
 
     request(endpoint, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        var json_response = (JSON.parse(response.body));
-        		console.log(json_response)
-        		switch (league){
+            switch (league){
 	        		case 'nba':
-	            	teams = formatNbaAndNflTeams(json_response);
-	            	break;
 	            case 'nfl':
-	            	teams = formatNbaAndNflTeams(json_response);
+	            	teams = formatNbaAndNflTeams(response.body);
 	            	break;
+              case 'mlb':
+                teaams = formatMlbTeams(response.body);
+                break;
 	           }
        mongoInsert(teams);
       }
@@ -67,18 +67,19 @@ switch (league){
   
 
   function mongoInsert(teams){
-  db.open(function(err, client){
-    client.collection("teams", function(err, col) {
-      for (var i = 0; i < teams.length; i++) {
-        //really the only 3 key:value pairs we care about for now
-        col.insert({team_id:teams[i].id, name:teams[i].name, market:teams[i].market}, function() {});
-      }
-    })
+    db.open(function(err, client){
+      client.collection("teams", function(err, col) {
+        for (var i = 0; i < teams.length; i++) {
+        //really the only 4 key:value pairs we care about for now
+          col.insert({team_id:teams[i].id, name:teams[i].name, market:teams[i].market, league:league}, function() {});
+        }
+      })
   });
 
 }
 
-var formatNbaAndNflTeams = function(hierarchy_response){
+var formatNbaAndNflTeams = function(response){
+  var  hierarchy_response = JSON.parse(response);
   for (i=0;i<hierarchy_response.conferences.length;i++){
     for (j=0;j<hierarchy_response.conferences[i].divisions.length;j++){
       for(k=0; k< hierarchy_response.conferences[i].divisions[j].teams.length; k++){
@@ -87,6 +88,20 @@ var formatNbaAndNflTeams = function(hierarchy_response){
     }   
   }
 return teams
+}
+
+var formatMlbTeams = function(response){
+  //convert the xml to json
+  parseString(response, function (err, result) {
+    //start teh loops
+     for (i=0; i < result[Object.keys(result)[0]].team.length;i++){
+      if (result[Object.keys(result)[0]].team[i].$.market.trim().length != 0){
+        teams.push(result[Object.keys(result)[0]].team[i].$);
+      }
+     }
+
+  });
+  return teams;
 }
 
 
