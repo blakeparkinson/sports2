30279c60bbc053406ab6328efbf48eabb4ecdd71
blakeparkinson <blake@edmodo.com>
2015-01-29 22:36:20 -0800

    Updating api keys and adjusting script

--- routes/teams.js
+++ routes/teams.js
@@ -1,3 +1,19 @@
+/// ----- KEYS -------///
+var nba_key = 'fp9yece877dze22evcbwz84q';
+var nfl_key = 'vsz2dyupsmnpsbyu7rjpqa8q';
+var ncaa_fb_key = 'qfsgey5amx9vm54he8ewxwfh';
+var mlb_key = '7gce52avk2cqmj65m5v7ff63';
+var ncaa_mb_key = 'xrnt5gsejfjkd8qz6nd8rtbx';
+var nhl_key = '6pmjhpkz4xt2e8q7pzn95wpy';
+var mma_key = 'fmzetam7v54nbrbaypgy9ycw';
+var ncaa_wb_key = 'nygw89t6gxzna6xgbbcchvmd';
+var soccer_wc_key = 'nw49yuqy98udtprsednz8t24';
+var soccer_na_key = 'hczs9xpjr3ffhbtshpj7r3n5';
+var nascar_key = 'de5k375fd658a7676494hdft';
+var golf_key = 'wuczn4z2ktufacuae7u8sxfc';
+/// ----------END KEYS-------------/////
+
+
 var express = require('express');
 var router = express.Router();
 var http = require("http"),
