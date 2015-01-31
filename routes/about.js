aaec37c9b42eed25d7716cb464eb1f28510dc815
HenryEdmodo <henry@edmodo.com>
2015-01-30 22:46:45 -0800

    no mongo
    bleh

--- routes/about.js
+++ routes/about.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var http = require("http"),
-    mongojs = require("mongojs"),
-    uri = 'mongodb://root:root@ds031541.mongolab.com:31541/rosterblitz';
-
-    db = mongojs.connect(uri);
+    
 
 router.get('/', function(res, res) {
       res.render('teams', {
