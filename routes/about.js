ec50de8b5dd0be9ad789b120047c718a67b9ea36
HenryEdmodo <henry@edmodo.com>
2015-01-30 22:42:59 -0800

    Can't add new page
    why not?

---
+++ routes/about.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+var http = require("http"),
+    mongojs = require("mongojs"),
+    uri = 'mongodb://root:root@ds031541.mongolab.com:31541/rosterblitz';
+
+    db = mongojs.connect(uri);
