c4a4be06264fed0f19264e28c8c4b2a7983e13b6
Kelsey <kelseyjgross@gmail.com>
2015-01-30 20:53:00 -0800

    db stuff minus response

--- routes/teams.js
+++ routes/teams.js
@@ -1,11 +1,14 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 var http = require("http"),
     mongojs = require("mongojs"),
     uri = 'mongodb://root:root@ds031541.mongolab.com:31541/rosterblitz';
 
     db = mongojs.connect(uri, ["teams"]);
-    db_players = mongojs.connect(uri, ["players"]);
+    db_players = mongojs.connect(uri, ["players"]),
+
+    nba_key = 'hdgj9e9vs9hquzc6ds22wtdy';
 
 
 
@@ -27,58 +30,46 @@ router.get('/team', function(req, res) {
 
 // when players endpoint is hit, call the API/DB using that team_id
 router.get('/players', function(req, res) {
-  temp = req.query.split('=');
-  team_id = temp[1];
+  team_id = req.query["team_id"];
   console.log("team id is: "+team_id);
-  fetchPlayers(team_id)
+  players = fetchPlayers(team_id);
+  res.json(players);
 });
 
-
-
-var fetchPlayers = function(team_id){
-  var mongoresult = db_players.players.find( { id: team_id } );  // Or however you query Mongo
-  var now = new Date();
-  var target = new Date(mongoresult.last_updated);
-  var daterange = now.getTime() - target.getTime();
-  // if no results or the results are more than 24 hours old...
-  if (mongoresult.length == 0 || daterange > 86400){
-    var players = fetchPlayersFromApi(team_id);
-    var teamDocument = formatPlayersDocument(team_id, players);
-    mongoInsertPlayers(teamDocument);
-    console.log(teamDocument);
-    return teamDocument;  //pass this over to client side to render
-  }
-  else { 
-    return mongoresult //already fresh in the DB so just pass this over to client side to render
-  }
   
+var fetchPlayers = function(team_id){ 
+      db_players.collection('players').find({team_id : team_id}).toArray(function (err, items){
+        secondFunction(items, team_id);
+      });  
 }
+
+
+var secondFunction = function(mongoresult, team_id){
+  if(typeof mongoresult !== "undefined"){
+      if (mongoresult == "[object Object]" || mongoresult  == "" /*|| daterange > 86400*/){
+        console.log("we in the if statement!");
+        var players = fetchPlayersFromApi(team_id); 
+        return players;    //pass this over to client side to render
+      }
+      else { 
+        console.log("we in the else statement!");
+        return mongoresult //already fresh in the DB so just pass this over to client side to render
+      }
+    }
+  else{
+    console.log("broke");
+  }
+}
+
 
 var fetchPlayersFromApi = function(team_id){
-  request('https://api.sportsdatallc.org/nba-t3/seasontd/2014/reg/teams/'+team_id+'/statistics.json?api_key='+nba_key)
+  request('https://api.sportsdatallc.org/nba-t3/seasontd/2014/reg/teams/'+team_id+'/statistics.json?api_key='+nba_key, function (error, response, body) {
     var json_response = (JSON.parse(response.body));
-    players = formatPlayers(json_response);
-    return players
-}
-
-
-function mongoInsertPlayers(players){
-  db_players.open(function(err, client){
-    client.collection("players", function(err, col) {
-     db_players.players.insert( players );
-    })
-  });
-
-}
-
-var formatPlayersDocument = function(team_id, players){
-  teamDocument = {};
-  teamDocument[team_id] = team_id;
-  teamDocument[id] = db.teams.find( { team_id: team_id }, { id: 1});
-  teamDocument[team_name] = db.teams.find( { team_id: team_id }, { name: 1});
-  teamDocument[market] = db.teams.find( { team_id: team_id }, { market: 1});
-  teamDocument[players] = players;
-  return teamDocument;
+    var players = formatPlayers(json_response);
+    var team = formatPlayersDocument(team_id, players)
+    mongoInsertPlayers(team);
+    return team;
+  });   
 }
 
 
@@ -107,18 +98,30 @@ var formatPlayers = function(response){
   }
   return playersarray;
 }
+  
+
+var formatPlayersDocument = function(team_id, players){
+  teamDocument = {};
+  teamDocument["team_id"] = team_id;  
+  //teamDocument[id] = db.teams.find( { team_id: team_id }, { id: 1});
+  //teamDocument[team_name] = db.teams.find( { team_id: team_id }, { name: 1});
+  //teamDocument[market] = db.teams.find( { team_id: team_id }, { market: 1});
+  teamDocument["players"] = players; 
+  //teamDocumnet[last_updated] = new date();
+  return teamDocument;
+}
 
 
+function mongoInsertPlayers(team_document){
+  console.log("inserting into the DB");
+  db_players.open(function(err, client){
+    client.collection("players", function(err, col) {
+      db_players.players.insert( team_document );
+    })
+  });
 
-
-
-
-
-
-
-
-
-
+  
+}
 
 
 module.exports = router;
