e133a5cf8bf189c37d54ff557885403ea6370e5e
blakeparkinson <blake@edmodo.com>
2015-02-18 01:29:02 -0800

    tabs

--- routes/quiz.js
+++ routes/quiz.js
@@ -3,24 +3,24 @@ var config = common.config();
 var express = require('express');
 var router = express.Router();
 var http = require("http");
-		mongojs = require("mongojs"),
+    mongojs = require("mongojs"),
     db = mongojs.connect(config.mongo_uri);
 var players_model = require('../models/players.js');
     
 
 router.get('/', function(req, res) {
-			res.quiz_page = true;
-			var quiz_id = req.query.quiz_id,
-					team_id = req.query.team_id,
-					league = req.query.league;
-			if (!team_id || !league){
-				  db.collection('quiz').findOne({_id : quiz_id},function (err, doc){
-				  	players = players_model.fetchPlayers(doc.team_id, doc.league, res, players_model.returnPlayers);
-					});
-			}
-			else{
-				players = players_model.fetchPlayers(team_id, league, res, players_model.returnPlayers);
-			}
+      res.quiz_page = true;
+      var quiz_id = req.query.quiz_id,
+          team_id = req.query.team_id,
+          league = req.query.league;
+      if (!team_id || !league){
+          db.collection('quiz').findOne({_id : quiz_id},function (err, doc){
+              players = players_model.fetchPlayers(doc.team_id, doc.league, res, players_model.returnPlayers);
+          });
+      }
+      else{
+        players = players_model.fetchPlayers(team_id, league, res, players_model.returnPlayers);
+      }
 
   });
 
