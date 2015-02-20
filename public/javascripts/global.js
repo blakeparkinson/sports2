95bc8972f82aa4ac62ae3abe32f943c536895d7b
Kelsey <kelseyjgross@gmail.com>
2015-02-19 18:16:35 -0800

    get rid of old code

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -167,34 +167,6 @@ function closePopupAndRefreshPage(){
   */
 }
 
-function fetchTeam(event) {
-    var target = $(event.target);
-    if (target.hasClass('quiz-btn')){
-      var team = $('.selected-team'),
-          id = team.data('id'),
-          team_id = team.data('team-id'),
-          league = team.data('league');
-
-    }
-    else{
-      var team_id = $('#teams option:selected').val();
-    }
-
-    var data = {team_id: team_id, league: league};
-
-    $.ajax({
-     	url: 'teams/players',
-      data: data,
-      type: 'get',
-      dataType: 'json',
-     		success: function(response){
-          window.location.href ='quiz?quiz_id=6'
-     		}
-    }).done(function() {
-});
-
-}
-
 function fetchQuiz(event) {
     var target = $(event.target);
     if (target.hasClass('quiz-btn')){
