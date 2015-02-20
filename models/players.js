b8b779c55f047b9fc2e5d4f4ce836db3f7072bf7
Kelsey <kelseyjgross@gmail.com>
2015-02-19 19:02:12 -0800

    api and mongo return same array of objects

--- models/players.js
+++ models/players.js
@@ -16,7 +16,7 @@ var players = [];
 var returnPlayers = function (players, res, league){
   if (res.quiz_page != undefined && res.quiz_page){
     res.render('quiz', {
-      players:players.players,
+      players:players,
       league: league
     });
 
@@ -77,32 +77,32 @@ switch (league){
                players_sorted = sortNBA(json_response);
                players = formatPlayers(players_sorted, team_id);
                mongoInsertPlayers(team_id, players);
-               callback(players, res, league)
+               callback(players.players, res, league)
                break;
                case 'nfl':
                json_response = JSON.parse(body);
                players_sorted = sortNFL(json_response);
                players = formatPlayers(players_sorted, team_id);
                mongoInsertPlayers(team_id, players);
-               callback(players, res, league)
+               callback(players.players, res, league)
                break;
                case 'nhl':
                 json_response = JSON.parse(body);
                 players = formatPlayers(json_response, team_id);
                 mongoInsertPlayers(team_id, players);
-                callback(players, res, league)
+                callback(players.players, res, league)
                 break;
                case 'eu_soccer':
                 playersParsed = formatEUSoccerPlayers(response.body);
                 players = formatPlayersDocument(team_id, playersParsed);
                 mongoInsertPlayers(team_id, players);
-                callback(players, res, league)
+                callback(players.players, res, league)
                 break;
                case 'mlb':  
                 playersParsed = formatMLBPlayers(response.body, team_id);
                 players = formatPlayersDocument(team_id, playersParsed);
                 mongoInsertPlayers(team_id, players);
-                callback(players, res, league)
+                callback(players.players, res, league)
                 break;
               }
       }
