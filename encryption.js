74b3e139ed22e30bf7f1ca20b3931b5335e350f4
Kelsey <kelseyjgross@gmail.com>
2015-02-24 18:16:40 -0800

    meh

--- encryption.js
+++ encryption.js
@@ -1,23 +1,60 @@
 var crypto = require('crypto');
 
 
+//  ORIGINAL 
+
 var encrypt = function(text){
-	var cipher1 = crypto.createCipher('aes256', 'password'); 
-	var cipherText1 = cipher1.update(text, 'ascii','binary'); 
-	cipherText1 += cipher1.final('binary'); 
-	console.log('Method 1 - text:' + text ); 
-	console.log('Method 1 - cipherText length:' + cipherText1.length); 
-	return cipherText1
+	var cipher = crypto.createCipher('aes192', 'mypassword');   // algorithm and password for encryption
+	var cipherText = cipher.update(text, 'utf8','binary');   // encrypt text from utf8 to binary
+	cipherText += cipher.final('binary'); 
+	var encoded = new Buffer(cipherText, 'binary').toString('base64');
+	console.log("cipherText length encrypted2"+encoded.length);
+	return encoded
+}
+
+/*var decrypt = function(encrypted_text){
+	console.log("decrypt length"+ encrypted_text.length);
+	var decipher = crypto.createDecipher('aes192', 'mypassword');    // algorithm and password for the decryption
+	var result = decipher.update(encrypted_text, 'binary', 'utf8');  // text to decrypt, from binary to utf8
+	result += decipher.final('utf8'); 
+	result = result.slice(1)
+	return result
+}*/
+
+var decrypt = function(encrypted_text){
+	console.log("decrypt length"+ encrypted_text.length);
+	encrypted_text = new Buffer(encrypted_text, 'base64').toString('binary');
+	var decipher = crypto.createDecipher('aes192', 'mypassword');    // algorithm and password for the decryption
+	var result = decipher.update(encrypted_text, 'binary', 'utf8');  // text to decrypt, from binary to utf8
+	//result += decipher.final('utf8'); 
+	console.log("result length"+ result.length);
+	return result
+}
+
+
+
+
+ // ONE ATTEMPT AT BUFFER
+/*var encrypt = function(text){
+	var cipher = crypto.createCipher('aes192', 'mypassword');   // algorithm and password for encryption
+	var cipherText = [cipher.update(new Buffer(text), 'utf8','binary')];   // encrypt text from utf8 to binary
+	cipherText.push(cipher.final('binary')); 
+	var encrypted;
+	encrypted = Buffer.concat(cipherText);
+	console.log("cipherText length encrypted"+encrypted.length);
+	return encrypted
 }
 
 var decrypt = function(encrypted_text){
-	var decipher1 = crypto.createDecipher('aes256', 'password'); 
-	var result1 = decipher1.update(encrypted_text, 'binary', 'ascii'); 
-	result1 += decipher1.final('ascii'); 
-	console.log('Method 1 - result:' + result1);
-	return result1
+	console.log("decrypt length"+ encrypted_text.length);
+	var decipher = crypto.createDecipher('aes192', 'mypassword');    // algorithm and password for the decryption
+	var result = [decipher.update(new Buffer(encrypted_text), 'binary', 'utf8')];  // text to decrypt, from binary to utf8
+	result.push(decipher.final('utf8'));
+	var decrypted = Buffer.concat(result).toString('utf8'); 
+	console.log("decrypted length "+decrypted.length)
+	return decrypted
 }
-
+*/
 
 
 module.exports = {
