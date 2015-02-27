3469d36e51a79941b57a0ac381c7a141510d8611
Kelsey <kelseyjgross@gmail.com>
2015-02-26 20:29:21 -0800

    hide key

--- encryption.js
+++ encryption.js
@@ -1,8 +1,9 @@
 var crypto = require('crypto');
-
+var common = require('./routes/common');
+var config = common.config();
 
 var encrypt = function(text){
-	var cipher = crypto.createCipher("bf-ecb", "key");  // algorithm and key. need to store key in config file
+	var cipher = crypto.createCipher("bf-ecb", config.encryption_key);  // algorithm and key. need to store key in config file
 	var data = cipher.update(text, "utf8", "base64");   
 	data += cipher.final("base64");   
 	return data
@@ -10,7 +11,7 @@ var encrypt = function(text){
 
 
 var decrypt = function(data){
-	var decipher = crypto.createDecipher("bf-ecb", "key");
+	var decipher = crypto.createDecipher("bf-ecb", config.encryption_key);
 	data = decipher.update(data, "base64", "utf8");
 	data += decipher.final("utf8");
 	return data
