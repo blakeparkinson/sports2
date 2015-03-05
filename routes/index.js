49499e13e8844b6b26b83c8e9ca9b05239cdcfd3
Kelsey <kelseyjgross@gmail.com>
2015-03-04 19:17:03 -0800

    sorted top 3 of week

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var common = require('./common')
 var config = common.config();
 var express = require('express');
 var router = express.Router();
+var _ = require('lodash');
 var http = require("http"),
     mongojs = require("mongojs"),
     db = mongojs.connect(config.mongo_uri);
@@ -16,21 +17,28 @@ router.get('/', function(req, res) {
 
 
 
-var popular_quizzes = db.collection('quiz').aggregate( [ 
-{ $project: {
-        _id: 0,
-        rb_team_id: 1,
-        Created_in_timeframe: {$cond: [{$lt: ['$created_at', oneWeekAgo]}, 1, 0]}
-        //    dateDifference:                 {$subtract: [ new Date(), "$date" ] } 
-        //Created_in_timeframe: {$cond: [{$gt: ['$created_at', {$subtract: [new Date(), 6048000000000000000000]}]}, 1, 0]}
-    }},
-
-{ 
-	$group : { _id : "$rb_team_id", counts : {$sum: '$Created_in_timeframe'} }   
-    } ], function(err, result){
-        console.log(result);
-        return result;
-    } );
+var popular_quizzes = db.collection('quiz').aggregate(  
+	[{ $project: {
+	        _id: 0,
+	        rb_team_id: 1,
+	        Created_in_timeframe: {$cond: [{$lt: ['$created_at', oneWeekAgo]}, 1, 0]}
+	    }
+	},
+	{ $group : { 
+			_id : "$rb_team_id", counts : {$sum: '$Created_in_timeframe'} 
+		}   
+	},
+	{ $sort: {
+	        counts: -1
+	    }
+	}], function(err, result){
+			newresult = [];
+	    	newresult.push(result.slice(0,3))
+	    	newresult = _.first(newresult)
+	    	console.log(newresult);
+	    	return newresult;
+	} 
+);
 
 
 
