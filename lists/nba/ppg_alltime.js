cbedc77a58d2643c7eca48b42753c11724e48d77
HenryEdmodo <henry@edmodo.com>
2015-03-04 21:29:49 -0600

    all time lists, nba
    ppg, apg, rpg, spg, bpg

---
+++ lists/nba/ppg_alltime.js
@@ -0,0 +1,62 @@
+var topPPG = [
+	{
+		rank: 1,
+		last_name: "Jordan",
+		first_name: "Michael",
+		ppg: 30.12
+	},
+	{
+		rank: 2,
+		last_name: "Chamberlain",
+		first_name: "Wilt",
+		ppg: 30.07
+	},
+	{
+		rank: 3,
+		last_name: "James",
+		first_name: "LeBron",
+		ppg: 27.44
+	},
+	{
+		rank: 4,
+		last_name: "Baylor",
+		first_name: "Elgin",
+		ppg: 27.36
+	},
+	{
+		rank: 5,
+		last_name: "Durant",
+		first_name: "Kevin",
+		ppg: 27.31
+	},
+	{
+		rank: 6,
+		last_name: "West",
+		first_name: "Jerry",
+		ppg: 27.03
+	},
+	{
+		rank: 7,
+		last_name: "Iverson",
+		first_name: "Allen",
+		ppg: 26.66
+	},
+	{
+		rank: 8,
+		last_name: "Pettit",
+		first_name: "Bob",
+		ppg: 26.36
+	},
+	{
+		rank: 9,
+		last_name: "Gervin",
+		first_name: "George",
+		ppg: 26.18
+	},
+	{
+		rank: 10,
+		last_name: "Oscar",
+		first_name: "Robertson",
+		ppg: 25.68
+	}
+]
