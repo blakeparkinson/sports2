d9f0691d58c47a3f358eb9d56be545ecd643da67
Kelsey <kelseyjgross@gmail.com>
2015-03-05 16:56:38 -0800

    clean tabs

--- routes/index.js
+++ routes/index.js
@@ -4,34 +4,34 @@ var express = require('express');
 var router = express.Router();
 var _ = require('lodash');
 var http = require("http"),
-    mongojs = require("mongojs"),
-    db = mongojs.connect(config.mongo_uri);
+ 	mongojs = require("mongojs"),
+	db = mongojs.connect(config.mongo_uri);
 var quizCutoffDate = new Date();
 quizCutoffDate.setDate(quizCutoffDate.getDate() - 7);  //currently set to 1 week ago
 
 
 router.get('/', function(req, res) {
 	var popular_quizzes = db.collection('quiz').aggregate(
-		[{ $project: {
-			_id: 0,
-			rb_team_id: 1,
-			Created_in_timeframe: {$cond: [{$lt: ['$created_at', quizCutoffDate]}, 1, 0]}
-			}
-		},
-		{ $group : {
-			_id : "$rb_team_id", counts : {$sum: '$Created_in_timeframe'} 
-			}
-		},
-		{ $sort: {
-			counts: -1
-			}
-		}], function(err, result){
-			newresult = [];
-			newresult.push(result.slice(0,3))
-			newresult = _.first(newresult)
-			console.log(newresult);
-			return newresult;
+	[{ $project: {
+		_id: 0,
+		rb_team_id: 1,
+		Created_in_timeframe: {$cond: [{$lt: ['$created_at', quizCutoffDate]}, 1, 0]}
 		}
+	},
+	{ $group : {
+		_id : "$rb_team_id", counts : {$sum: '$Created_in_timeframe'} 
+		}
+	},
+	{ $sort: {
+		counts: -1
+		}
+	}], function(err, result){
+		newresult = [];
+		newresult.push(result.slice(0,3))
+		newresult = _.first(newresult)
+		console.log(newresult);
+		return newresult;
+	}
 	);
 	res.render('index', {session:req.session});
 });
