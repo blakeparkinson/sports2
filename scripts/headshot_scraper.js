16a3266e0ec4787e6984cfe5cea7e4bf8148f1f3
blakeparkinson <blake@edmodo.com>
2015-03-24 12:41:49 -0700

    saving stuff

--- scripts/headshot_scraper.js
+++ scripts/headshot_scraper.js
@@ -7,19 +7,38 @@ var fs = require('fs');
 var players_model = require('../models/players.js');
 var http = require("http"),
     mongojs = require("mongojs"),
+        async = require('async'),
+
     db = mongojs.connect(config.mongo_uri);
 var images_list = require('../lists/images2.js');
 
-var download = function(uri, filename, callback){
+var download = function(uri, filename, player, items, player_type, iteration){
   request.head(uri, function(err, res, body){
+    var avatar_url = {};
     console.log('content-type:', res.headers['content-type']);
     var content_type = res.headers['content-type'];
     //make sure the image actually exists
     if (content_type == 'image/jpeg'){
-    	request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+        avatar_url[player_type+"."+ iteration +".avatar_url"] = '../images/headshots/nba/'+player.full_name.replace(/\s+/g, '-').toLowerCase()+'.jpg';
+    	request(uri).pipe(fs.createWriteStream(filename)).on('close', function(){
+        });
+        
     }
+    else{
+        var team_id = items.team_id;
+        avatar_url[player_type+"."+ iteration +".avatar_url"] = null;
+    }
+    db.collection('players').findAndModify({
+            query: {team_id: team_id},
+            update: {$set: avatar_url},
+            new: true
+        }, function(err, doc, lastErrorObject) {
+            console.log(doc);
+        });
+    
   });
 };
+
 
 db.collection('players').find().toArray(function (err, items){
     for (var i=0; i < items.length; i++){
@@ -36,17 +55,13 @@ db.collection('players').find().toArray(function (err, items){
     	for (var b=0; b < items[i].bench.length; b++){
     		var full_name = items[i].bench[b].full_name.replace(/\s+/g, '-').toLowerCase();
     		var url = 'http://www.gannett-cdn.com/media/SMG/sports_headshots/nba/player/2014/'+usat_id+'/120x120/'+full_name+'.jpg';
-	  		console.log(url);
-	  		download(url, '../images/headshots/nba/'+full_name+'.jpg', function(){
-					console.log('done');
-				});
+            download(url, '../images/headshots/nba/'+full_name+'.jpg', items[i].bench[b], items[i], 'bench', b);
+
     	}
-    	for (var b=0; b < items[i].starters.length; b++){
-    		var full_name = items[i].starters[b].full_name.replace(/\s+/g, '-').toLowerCase();
+    	for (var c=0; c < items[i].starters.length; c++){
+    		var full_name = items[i].starters[c].full_name.replace(/\s+/g, '-').toLowerCase();
     		var url = 'http://www.gannett-cdn.com/media/SMG/sports_headshots/nba/player/2014/'+usat_id+'/120x120/'+full_name+'.jpg';
-	  		download(url, '../images/headshots/nba/'+full_name+'.jpg', function(){
-					console.log('done');
-				});
+	  		download(url, '../images/headshots/nba/'+full_name+'.jpg', items[i].starters[c], items[i], 'starters', c);
     	}
 
     	//TODO go through and insert the picture back to the player, with empty image for ppl we dont find
