3df16b5904e3028632accb79ffe857f33089480e
blakeparkinson <blake@edmodo.com>
2015-03-30 23:58:42 -0700

    Extending the positional helper to account for soccer

--- app.js
+++ app.js
@@ -139,27 +139,52 @@ hbs.registerHelper('inc', function(value, options)
     return parseInt(value) + 1;
 });
 
-hbs.registerHelper('render_position', function(position){
+hbs.registerHelper('render_position', function(league, position){
+
+  if (arguments.length < 2) {
+        throw new Error("Handlerbars Helper 'render_position' needs 2 parameters");
+    }
 
   var position = position.toUpperCase();
-  switch (position){
-    case 'G':
-      position = 'Guard';
+  switch (league){
+    case 'nba':
+      switch (position){
+        case 'G':
+          position = 'Guard';
+        break;
+        case 'F':
+          position = 'Forward';
+        break;
+        case 'C':
+          position = 'Center';
+        break;
+        case 'F-C':
+        case 'C-F':
+         position = 'Forward/Center';
+        break;
+        case 'G-F':
+        case 'F-G':
+         position = 'Guard/Forward';
+        break;
+      }
     break;
-    case 'F':
-      position = 'Forward';
-    break;
-    case 'C':
-      position = 'Center';
-    break;
-    case 'F-C':
-    case 'C-F':
-     position = 'Forward/Center';
-    break;
-    case 'G-F':
-    case 'F-G':
-     position = 'Guard/Forward';
-    break;
+    case 'eu_soccer':
+      switch (position){
+        case 'D': 
+          position = 'Defender';
+        break;
+        case 'F':
+          position = 'Forward';
+        break;
+        case 'M':
+          position = 'Midfielder';
+        break;
+        case 'G':
+          position = 'Goalie';
+        break;
+
+      }
+      break;
   }
   return position;
 })
