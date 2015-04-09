b5676037fe1c3ce565c087fef2ef51e835b8e5ed
blakeparkinson <blake@edmodo.com>
2015-04-08 18:04:13 -0700

    Add some more stuff to the player object

--- models/players.js
+++ models/players.js
@@ -152,7 +152,6 @@ switch (league){
         switch (league){
           case 'nba':
             json_response = JSON.parse(body);
-
             // find the image from usat and insert into players array
             for (a=0;a<Object.keys(json_response.players).length;a++){
               var player_name = json_response.players[a].first_name.toLowerCase()+'-'+json_response.players[a].last_name.toLowerCase()
@@ -164,6 +163,7 @@ switch (league){
             request('https://api.sportsdatallc.org/nba-t3/teams/'+encryption.decrypt(team_id)+'/profile.json?api_key=' +config.nba_key, function (error, response, roster) {
                   if (!error && response.statusCode == 200) {
                      var team_roster = JSON.parse(roster);
+                     console.log(team_roster);
                      var players_roster = team_roster.players;
                      var team_name = team_roster.market + ' ' + team_roster.name;
                      for (var i=0; i<json_response.players.length;i++){
@@ -171,6 +171,10 @@ switch (league){
                         //compare by player id
                         if (json_response.players[i].id == players_roster[j].id){
                           json_response.players[i].status = players_roster[j].status;
+                          json_response.players[i].weight = players_roster[j].weight;
+                          json_response.players[i].height = players_roster[j].height;
+                          json_response.players[i].experience = players_roster[j].experience;
+                          json_response.players[i].college = players_roster[j].college;
                           //we found a match, break out of the 2nd loop iteration
                           continue;
                         }
