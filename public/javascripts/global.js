3648a96eb57fe14ab5030caa300a211b6c4e770b
blakeparkinson <blake@edmodo.com>
2015-04-16 00:13:46 -0700

    Revert "Clicking a team from leaguesearchpage creates a quiz"
    This reverts commit 5dc635c0ba2a3381a7ea980a3e521a8b6a48456d.

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -218,23 +218,15 @@ function fetchQuiz(event) {
 }
 
 function AjaxCreateQuiz(data){
-  var url = window.location.pathname.split("/");
-  if (url.length > 2){
-    //we are in an action page (i.e. leaguesearch/nba, go up a level)
-    var ajax_url = '../teams/quiz'
-  }
-  else{
-    var ajax_url = 'teams/quiz'
-  }
 
     $.ajax({
-      url: ajax_url,
+      url: 'teams/quiz',
       data: data,
       type: 'get',
       dataType: 'json',
         success: function(response){
           if (!response.error){
-            window.location.href = '../quiz?id='+response["_id"]+'&team_id='+response["rb_team_id"]+'&league='+response["league"];
+            window.location.href = 'quiz?id='+response["_id"]+'&team_id='+response["rb_team_id"]+'&league='+response["league"];
           }
           else{console.log('Throw an error popup or smething eventually');}
         }
