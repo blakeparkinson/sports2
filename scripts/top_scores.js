f57d6a3cbb8f149d7fe900d13138a0cf2c70daba
blakeparkinson <blake@edmodo.com>
2015-04-20 23:32:18 -0700

    tabs

--- scripts/top_scores.js
+++ scripts/top_scores.js
@@ -14,25 +14,25 @@ var app     = express();
             $('.data').filter(function(){
 
            // Let's store the data we filter into a variable so we can easily see what's going on.
-           			
+                 
 
                 var data = $(this);
                 var results = [];
                 var tr = data.find('.row1, .row2')
                 tr.each(function(i, element){
-                	var player = $(this).find('td:first-child').text();
-                	var ppg = $(this).find('td.sort').text();
-                	var metadata = {
-        						name: player,
-        						ppg: ppg,
-      						};
-      						results.push(metadata);
+                  var player = $(this).find('td:first-child').text();
+                  var ppg = $(this).find('td.sort').text();
+                  var metadata = {
+                    name: player,
+                    ppg: ppg,
+                  };
+                  results.push(metadata);
                 })
                 console.log(results);
 
             })
         }
         else{
-        	conole.log(error);
+          conole.log(error);
         }
     })
