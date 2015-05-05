a47e66925cec242c70ef075d9624a0e3af4065f7
HenryEdmodo <henry@edmodo.com>
2015-05-04 17:40:53 -0700

    list style and descriptions
    done

--- models/teams.js
+++ models/teams.js
@@ -21,5 +21,46 @@ var deleteTeam = function(data){
 }
 
 module.exports = {
-  deleteTeam: deleteTeam
+  deleteTeam: deleteTeam,
+  fetchStatDescription: fetchStatDescription
+}
+
+/** TODO HAVE HONREE POPULATE DESCRIPTIONS WITH THIS FUNCTION **/
+var fetchStatDescription = function(stat, listType, league){
+  switch(stat) {
+    case "ppg": //points per game
+        description = 'Points per game';
+        break;
+      case "rpg": //rebounds
+        description = 'Rebounds per game';
+        break;
+      case "fieldGoalPercentage": //fieldgoals
+        description = 'Field goal percentage';
+        break;
+      case "ftPercentage": //free throw
+        description = 'Free throw percentage';
+        break;
+      case "threePointPercentage": //three point
+        description = 'Three points percentage';
+        break;
+      case "apg": //assists
+        description = 'Assists per game';
+        break;
+      case "spg": //steals
+        description = 'Steals per game';
+        break;
+      case "bpg": //blocks
+        description = 'Blocks per game';
+        break;
+      case "tpg": //turnovers
+        description = 'Turnovers per game';
+        break;
+      case "fpg": //fouls
+        description = 'Fouls per game';
+        break;
+      case "mpg": //minutes
+        description = 'Minutes per game';
+        break;
+  }
+  return description;
 }
