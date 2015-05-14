b45620bf0039bf7753a23bd21cf2032687709595
blakeparkinson <blake@edmodo.com>
2015-05-13 18:05:36 -0700

    comments/tabs

--- test/test.js
+++ test/test.js
@@ -1,16 +1,19 @@
+//to run tests type "npm test" from the root dir
+
 var assert = require("assert");
 var players_model = require('../models/players.js');
 var _ = require('lodash');
 
 
 describe('tests', function(){
-	describe('pics', function(){
-		it('should return an image matching the corresponding league', function(){
-			var leagues = ['nba', 'mlb', 'nfl', 'nhl'];
-			for (var i =0; i < leagues.length; i++){
-				var img = players_model.randImg(leagues[i]);
-				assert.notEqual(-1,img.indexOf(leagues[i]));
-			}
-		})
-	})
+  describe('pics', function(){
+    it('should return an image matching the corresponding league', function(){
+      var leagues = ['nba', 'mlb', 'nfl', 'nhl'];
+      for (var i =0; i < leagues.length; i++){
+        var img = players_model.randImg(leagues[i]);
+        //the image should have the league text ('nba, 'mlb' ,'nhl', etc...) in the name
+        assert.notEqual(-1,img.indexOf(leagues[i]));
+      }
+    })
+  })
 })
