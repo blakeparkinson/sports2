60d9fdb49dd450a1ba55076cb4ef1722c2c00a67
HenryEdmodo <henry@edmodo.com>
2015-05-20 15:56:53 -0700

    nhl team colors
    - change file you’re reading from to all_team_colors.js
    
    i hate CW

---
+++ lists/team_colors/all_team_colors.js
@@ -0,0 +1,586 @@
+var team_colors = [
+  {
+    "league": "nba",
+    "teams": [
+      {
+        "team_id": "7yZHDWViK",
+        "team_name": "Atlanta Hawks",
+        "market": "Atlanta",
+        "name": "Hawks",
+        "primary_hex": "#000080",
+        "secondary_hex": "#FF0000"
+      },
+      {
+        "team_id": "m1wBwbNst",
+        "team_name": "Boston Celtics",
+        "market": "Boston",
+        "name": "Celtics",
+        "primary_hex": "#009E60",
+        "secondary_hex": "#000000"
+      },
+      {
+        "team_id": "mklSD-EjY",
+        "team_name": "Charlotte Hornets",
+        "market": "Charlotte",
+        "name": "Hornets",
+        "primary_hex": "#1D1160",
+        "secondary_hex": "#008CA8"
+      },
+      {
+        "team_id": "m1LrD-NiY",
+        "team_name": "Brooklyn Nets",
+        "market": "Brooklyn",
+        "name": "Nets",
+        "primary_hex": "#000000",
+        "secondary_hex": "#FFFFFF"
+      },      
+      {
+        "team_id": "mkFBD-NiF",
+        "team_name": "Chicago Bulls",
+        "market": "Chicago",
+        "name": "Bulls",
+        "primary_hex": "#D4001F",
+        "secondary_hex": "#000000"
+      },
+      {
+        "team_id": "mk9SPZViF",
+        "team_name": "Cleveland Cavaliers",
+        "market": "Cleveland",
+        "name": "Cavaliers",
+        "primary_hex": "#860038",
+        "secondary_hex": "#FDBB30"
+      },
+      {
+        "team_id": "QyvgrDWViY",
+        "team_name": "Dallas Mavericks",
+        "market": "Dallas",
+        "name": "Mavericks",
+        "primary_hex": "#0B60AD",
+        "secondary_hex": "#A9A9A9"
+      },
+      {
+        "team_id": "QJzxSPWViK",
+        "team_name": "Denver Nuggets",
+        "market": "Denver",
+        "name": "Nuggets",
+        "primary_hex": "#4B90CD",
+        "secondary_hex": "#FDB827"
+      },
+      {
+        "team_id": "mJ3HD-NoY",
+        "team_name": "Detroit Pistons",
+        "market": "Detroit",
+        "name": "Pistons",
+        "primary_hex": "#00519A",
+        "secondary_hex": "#EB003C"
+      },
+      {
+        "team_id": "Qk4lBwbVst",
+        "team_name": "Houston Rockets",
+        "market": "Houston",
+        "name": "Rockets",
+        "primary_hex": "#CE1138",
+        "secondary_hex": "#CCCCCC"
+      },
+      {
+        "team_id": "X1dxrP-VjY",
+        "team_name": "Golden State Warriors",
+        "market": "Golden State",
+        "name": "Warriors",
+        "primary_hex": "#04529C",
+        "secondary_hex": "#FFCC33"
+      },
+      {
+        "team_id": "7JsHvWViY",
+        "team_name": "Indiana Pacers",
+        "market": "Indiana",
+        "name": "Pacers",
+        "primary_hex": "#092C57",
+        "secondary_hex": "#FFC322"
+      },
+      {
+        "team_id": "X15lBDbNoY",
+        "team_name": "Los Angeles Clippers",
+        "market": "Los Angeles",
+        "name": "Clippers",
+        "primary_hex": "#EE2944",
+        "secondary_hex": "#146AA2"
+      },
+      {
+        "team_id": "QkYgBDb4sY",
+        "team_name": "Los Angeles Lakers",
+        "market": "Los Angeles",
+        "name": "Lakers",
+        "primary_hex": "#4A2583",
+        "secondary_hex": "#F5AF1B"
+      },
+      {
+        "team_id": "XJ7erwZVsK",
+        "team_name": "Memphis Grizzlies",
+        "market": "Memphis",
+        "name": "Grizzlies",
+        "primary_hex": "#001F70",
+        "secondary_hex": "#7399C6"
+      },
+      {
+        "team_id": "XJfrPZNjY",
+        "team_name": "Miami Heat",
+        "market": "Miami",
+        "name": "Heat",
+        "primary_hex": "#000000",
+        "secondary_hex": "#B62630"
+      },
+      {
+        "team_id": "7kaBD-Eot",
+        "team_name": "Milwaukee Bucks",
+        "market": "Milwaukee",
+        "name": "Bucks",
+        "primary_hex": "#003614",
+        "secondary_hex": "#E32636"
+      },
+      {
+        "team_id": "QkASwZNoF",
+        "team_name": "Minnesota Timberwolves",
+        "market": "Minnesota",
+        "name": "Timberwolves",
+        "primary_hex": "#0F4D92",
+        "secondary_hex": "#50C878"
+      },
+       {
+        "team_id": "mkHxHwbNoF",
+        "team_name": "New Orleans Pelicans",
+        "market": "New Orleans",
+        "name": "Pelicans",
+        "primary_hex": "#002B5C",
+        "secondary_hex": "#E31836"
+      },
+       {
+        "team_id": "QJ4HPW4jt",
+        "team_name": "New York Knicks",
+        "market": "New York",
+        "name": "Knicks",
+        "primary_hex": "#0953A0",
+        "secondary_hex": "#FF7518"
+      },
+       {
+        "team_id": "Q1lgrP-4jt",
+        "team_name": "Oklahoma City Thunder",
+        "market": "Oklahoma City",
+        "name": "Thunder",
+        "primary_hex": "#007DC3",
+        "secondary_hex": "#F05133"
+      },
+       {
+        "team_id": "Qk7rPZ4iY",
+        "team_name": "Orlando Magic",
+        "market": "Orlando",
+        "name": "Magic",
+        "primary_hex": "#0047AB",
+        "secondary_hex": "#708090"
+      },
+       {
+        "team_id": "QJrrvbNsK",
+        "team_name": "Philadelphia 76ers",
+        "market": "Philadelphia",
+        "name": "76ers",
+        "primary_hex": "#0046AD",
+        "secondary_hex": "#D0103A"
+      },
+       {
+        "team_id": "QkjlHvbNsK",
+        "team_name": "Phoenix Suns",
+        "market": "Phoenix",
+        "name": "Suns",
+        "primary_hex": "#1C105E",
+        "secondary_hex": "#E65F20"
+      },
+      {
+        "team_id": "XkWxBvWNoY",
+        "team_name": "Portland Trail Blazers",
+        "market": "Portland",
+        "name": "Trail Blazers",
+        "primary_hex": "#F0163A",
+        "secondary_hex": "#B6BFBF"
+      },
+      {
+        "team_id": "XkhgSwZ4oK",
+        "team_name": "Sacramento Kings",
+        "market": "Sacramento",
+        "name": "Kings",
+        "primary_hex": "#753BBD",
+        "secondary_hex": "#8A8D8F"
+      },
+      {
+        "team_id": "QyIeSw-EsF",
+        "team_name": "San Antonio Spurs",
+        "market": "San Antonio",
+        "name": "Spurs",
+        "primary_hex": "#000000",
+        "secondary_hex": "#BEC8C9"
+      },
+      {
+        "team_id": "Q1_BvW4jF",
+        "team_name": "Toronto Raptors",
+        "market": "Toronto",
+        "name": "Raptors",
+        "primary_hex": "#B31B1B",
+        "secondary_hex": "#708090"
+      },
+      {
+        "team_id": "X11lBPW4iY",
+        "team_name": "Utah Jazz",
+        "market": "Utah",
+        "name": "Jazz",
+        "primary_hex": "#00275D",
+        "secondary_hex": "#B5B5B5"
+      },
+      {
+        "team_id": "Q1BvbViF",
+        "team_name": "Washington Wizards",
+        "market": "Washington",
+        "name": "Wizards",
+        "primary_hex": "#C60C30",
+        "secondary_hex": "#002244"
+      }
+    ]  
+    {
+    "league": "nhl",
+    "teams": [
+      {
+        "_id": "71-qFHbvC",
+        "team_id": "sRz+v62f37sNqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Sharks",
+        "market": "San Jose",
+        "league": "nhl",
+        "usat_id": "SJ",
+        "type": "roster",
+        "primary_hex": "#05535D",
+        "secondary_hex": "#000000"
+      },
+      {
+        "_id": "X1I9YrZDC",
+        "team_id": "OvSor5Pb4IMNqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Stars",
+        "market": "Dallas",
+        "league": "nhl",
+        "usat_id": "DAL",
+        "type": "roster",
+        "primary_hex": "#006A4E",
+        "secondary_hex": "#C0C0C0"
+      }.
+      {
+        "_id": "Xki9YHWDA",
+        "team_id": "dw53EN1CMekNqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Blues",
+        "market": "St. Louis",
+        "league": "nhl",
+        "usat_id": "STL",
+        "type": "roster",
+        "primary_hex": "#0546A0",
+        "secondary_hex": "#FFC325"
+      },
+      {
+        "_id": "Xygx5FHWvA",
+        "team_id": "sqGm1C2DT54NqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Senators",
+        "market": "Ottawa",
+        "league": "nhl",
+        "usat_id": "OTT",
+        "type": "roster",
+        "primary_hex": "#E4173E",
+        "secondary_hex": "#D69F0F"
+      },
+      {
+        "_id": "7yrl9tBWwR",
+        "team_id": "5/DoRlJdgMQNqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Blue Jackets",
+        "market": "Columbus",
+        "league": "nhl",
+        "usat_id": "CBJ",
+        "type": "roster",
+        "primary_hex": "#0546A0",
+        "secondary_hex": "#FFC325"
+      },
+      {
+        "_id": "my5gcFSbPC",
+        "team_id": "MZclMwsejnINqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Penguins",
+        "market": "Pittsburgh",
+        "league": "nhl",
+        "usat_id": "PIT",
+        "type": "roster",
+        "primary_hex": "#000000",
+        "secondary_hex": "#D1BD80"
+      },
+      {
+        "_id": "mkcYSbP0",
+        "team_id": "Mv0pfAeKAbUNqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Kings",
+        "market": "Los Angeles",
+        "league": "nhl",
+        "usat_id": "LA",
+        "type": "roster",
+        "primary_hex": "#000000",
+        "secondary_hex": "#AFB7BA"
+      },
+      {
+        "_id": "QJE9FSZP0",
+        "team_id": "dftDu05rrtQNqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Oilers",
+        "market": "Edmonton",
+        "league": "nhl",
+        "usat_id": "EDM",
+        "type": "roster",
+        "primary_hex": "#003777",
+        "secondary_hex": "#E66A20"
+      },
+      {
+        "_id": "m1F5tB-vR",
+        "team_id": "SOh4cUJFTrMNqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Blackhawks",
+        "market": "Chicago",
+        "league": "nhl",
+        "usat_id": "CHI",
+        "type": "roster",
+        "primary_hex": "#E3263A",
+        "secondary_hex": "#000000"
+      },
+      {
+        "_id": "XkAcKBbwA",
+        "team_id": "zCUjgU6znWINqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Bruins",
+        "market": "Boston",
+        "league": "nhl",
+        "usat_id": "BOS",
+        "type": "roster",
+        "primary_hex": "#000000",
+        "secondary_hex": "#FFC422"
+      },
+      {
+        "_id": "Xkmg5YrWP0",
+        "team_id": "gcduz7AHFcMNqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Lightning",
+        "market": "Tampa Bay",
+        "league": "nhl",
+        "usat_id": "TB",
+        "type": "roster",
+        "primary_hex": "#013E7D",
+        "secondary_hex": "#C0C0C0"
+      },
+      {
+        "_id": "X1ueqFBZvA",
+        "team_id": "8zTg44C0vWUNqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Rangers",
+        "market": "New York",
+        "league": "nhl",
+        "usat_id": "NYR",
+        "type": "roster",
+        "primary_hex": "#0161AB",
+        "secondary_hex": "#E6393F"
+      },
+      {
+        "_id": "7JGcFrWwR",
+        "team_id": "5ruVgmp8JdENqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Flames",
+        "market": "Calgary",
+        "league": "nhl",
+        "usat_id": "CGY",
+        "type": "roster",
+        "primary_hex": "#E03A3E",
+        "secondary_hex": "#FFC758"
+      },
+      {
+        "_id": "XJv9KBWvR",
+        "team_id": "+lz5EgOZoboNqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Avalanche",
+        "market": "Colorado",
+        "league": "nhl",
+        "usat_id": "COL",
+        "type": "roster",
+        "primary_hex": "#8B2942",
+        "secondary_hex": "#01548A"
+      },
+      {
+        "_id": "QJ3qtSbP0",
+        "team_id": "cp5JZ8eXuxoNqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Jets",
+        "market": "Winnipeg",
+        "league": "nhl",
+        "usat_id": "WPG",
+        "type": "roster",
+        "primary_hex": "#002E62",
+        "secondary_hex": "#A8A9AD"
+      },
+      {
+        "_id": "my-eqYrbP0",
+        "team_id": "OmzhGDkoIZENqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Canadiens",
+        "market": "Montreal",
+        "league": "nhl",
+        "usat_id": "MTL",
+        "type": "roster",
+        "primary_hex": "#BF2F38",
+        "secondary_hex": "#213770"
+      },
+      {
+        "_id": "XkIgcKHWvR",
+        "team_id": "mNtNgj1VuVwNqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Devils",
+        "market": "New Jersey",
+        "league": "nhl",
+        "usat_id": "NJ",
+        "type": "roster",
+        "primary_hex": "#E03A3E",
+        "secondary_hex": "#000000"
+      },
+      {
+        "_id": "7yseqKrZvC",
+        "team_id": "XosTDLYChBQNqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Capitals",
+        "market": "Washington",
+        "league": "nhl",
+        "usat_id": "WSH",
+        "type": "roster",
+        "primary_hex": "#CF132B",
+        "secondary_hex": "#00214E"
+      },
+      {
+        "_id": "mJx9YrbvA",
+        "team_id": "zzmzOxPX/YMNqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Coyotes",
+        "market": "Arizona",
+        "league": "nhl",
+        "usat_id": "ARI",
+        "type": "roster",
+        "primary_hex": "#841F27",
+        "secondary_hex": "#000000"
+      },
+      {
+        "_id": "XJS9tS-DR",
+        "team_id": "oH7CVm5t/kUNqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Ducks",
+        "market": "Anaheim",
+        "league": "nhl",
+        "usat_id": "ANA",
+        "type": "roster",
+        "primary_hex": "#91764B",
+        "secondary_hex": "#000000"
+      },
+      {
+        "_id": "mJ9cYrbwR",
+        "team_id": "SnMqcXyH2s8NqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Predators",
+        "market": "Nashville",
+        "league": "nhl",
+        "usat_id": "NSH",
+        "type": "roster",
+        "primary_hex": "#FDBB2F",
+        "secondary_hex": "#002E62"
+      },
+      {
+        "_id": "mJJxcYHWDA",
+        "team_id": "1eNWRAC4Z2ANqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Sabres",
+        "market": "Buffalo",
+        "league": "nhl",
+        "usat_id": "BUF",
+        "type": "roster",
+        "primary_hex": "#002E62",
+        "secondary_hex": "#FDBB2F"
+      },
+      {
+        "_id": "XkElqYHWDA",
+        "team_id": "yZ965PPShwkNqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Panthers",
+        "market": "Florida",
+        "league": "nhl",
+        "usat_id": "FLA",
+        "type": "roster",
+        "primary_hex": "#C8213F",
+        "secondary_hex": "#002E5F"
+      },
+      {
+        "_id": "QJYgqFH-D0",
+        "team_id": "a1+u0VqaWQoNqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Flyers",
+        "market": "Philadelphia",
+        "league": "nhl",
+        "usat_id": "PHI",
+        "type": "roster",
+        "primary_hex": "#F47940",
+        "secondary_hex": "#000000"
+      },
+      {
+        "_id": "mJX9Yr-PA",
+        "team_id": "HUI+0GYoW9UNqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Canucks",
+        "market": "Vancouver",
+        "league": "nhl",
+        "usat_id": "VAN",
+        "type": "roster",
+        "primary_hex": "#07346F",
+        "secondary_hex": "#047A4A"
+      },
+      {
+        "_id": "Q1u5Fr-wC",
+        "team_id": "JNSpIUAsv+4NqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Wild",
+        "market": "Minnesota",
+        "league": "nhl",
+        "usat_id": "MIN",
+        "type": "roster",
+        "primary_hex": "#025736",
+        "secondary_hex": "#BF2B37"
+      },
+      {
+        "_id": "7yT9KrWvR",
+        "team_id": "yGoMQQMiuxwNqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Red Wings",
+        "market": "Detroit",
+        "league": "nhl",
+        "usat_id": "DET",
+        "type": "roster",
+        "primary_hex": "#EC1F26",
+        "secondary_hex": "#ffffff"
+      },
+      {
+        "_id": "X1zx9tSWPC",
+        "team_id": "thA4Y8uC5O8NqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Maple Leafs",
+        "market": "Toronto",
+        "league": "nhl",
+        "usat_id": "TOR",
+        "type": "roster",
+        "primary_hex": "#003777",
+        "secondary_hex": "#ffffff"
+      },
+      {
+        "_id": "mywgctS-wR",
+        "team_id": "PM3z510nNMwNqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Islanders",
+        "market": "New York",
+        "league": "nhl",
+        "usat_id": "NYI",
+        "type": "roster",
+        "primary_hex": "#00529B",
+        "secondary_hex": "#F57D31"
+      },
+      {
+        "_id": "Qy2xctS-DR",
+        "team_id": "exWXQ0tPjCQNqWoobhugKyra4CJ9d52ydUbXOJSZto8QMO+h/Pr/Pg==",
+        "name": "Hurricanes",
+        "market": "Carolina",
+        "league": "nhl",
+        "usat_id": "CAR",
+        "type": "roster",
+        "primary_hex": "#E03A3E",
+        "secondary_hex": "#000000"
+      }
+    ]             
+  }            
+  }
+]
+
+module.exports = {
+  team_colors: team_colors
+}
