661227fec0acab87a1c218c11ac4f02bbb5773b4
Kelsey <kelseyjgross@gmail.com>
2015-06-04 18:31:22 -0700

    everything but node modules

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+// Include gulp
+var gulp = require('gulp'); 
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var minifyCss = require('gulp-minify-css');
+
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Compile Our CSS
+gulp.task('minify-css', function() {
+  return gulp.src('public/stylesheets/*.css')
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('public/test'));
+});
+
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('public/javascripts/*.js')
+        .pipe(concat('all.js'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/test'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('js/*.js', ['lint', 'scripts']);
+    gulp.watch('scss/*.scss', ['minify-css']);
+});
+
+// Default Task
+gulp.task('default', ['lint', 'minify-css', 'scripts', 'watch']);
+
+
+
