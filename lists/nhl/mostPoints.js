010326d48411b793ad72e909ed72be3a0761e909
HenryEdmodo <henry@edmodo.com>
2015-06-09 19:22:28 -0700

    most points list NHL
    add it

---
+++ lists/nhl/mostPoints.js
@@ -0,0 +1,94 @@
+var mostPoints = [
+  {
+  "league": "nhl",
+  "type":"goats",
+  "keywords": "nhl points points hockey",
+  "category": "mostGoals",
+  "description": "Most Points in NHL History",
+  "players": [
+    {
+        "player_id": "1",
+        "rank": "1",
+        "last_name": "Gretzky",
+        "first_name": "Wayne",
+        "full_name":"Wayne Gretzky",
+        "stat": "2857  "
+    },
+    {
+        "player_id": "2",
+        "rank": "2",
+        "last_name": "Messier",
+        "first_name": "Mark",
+        "full_name":"Mark Messier",
+        "stat": "1887"
+    },
+    {
+        "player_id": "3",
+        "rank": "3",
+        "last_name": "Howe",
+        "first_name": "Gordie",
+        "full_name":"Gordie Howe",
+        "stat": "1850"
+    },
+    {
+        "player_id": "4",
+        "rank": "4",
+        "last_name": "Jaromir",
+        "first_name": "Jagr",
+        "full_name":"Marcel Dionne",
+        "stat": "1802"
+    },
+    {
+        "player_id": "5",
+        "rank": "5",
+        "last_name": "Francis",
+        "first_name": "Ron",
+        "full_name":"Ron Francis",
+        "stat": "1802"
+    },
+    {
+        "player_id": "6",
+        "rank": "6",
+        "last_name": "Esposito",
+        "first_name": "Phil",
+        "full_name":"Phil Esposito",
+        "stat": "1798"
+    },
+    {
+        "player_id": "7",
+        "rank": "7",
+        "last_name": "Yzerman",
+        "first_name": "Steve",
+        "full_name":"Steve Yzerman",
+        "stat": "1755"
+    },
+    {
+        "player_id": "8",
+        "rank": "8",
+        "last_name": "Lemieux",
+        "first_name": "Mario",
+        "full_name":"Mario Lemieux",
+        "stat": "1723"
+    },
+    {
+        "player_id": "9",
+        "rank": "9",
+        "last_name": "Sakic",
+        "first_name": "Joe",
+        "full_name":"Joe Sakic",
+        "stat": "1641"
+    },
+    {
+        "player_id": "10",
+        "rank": "10",
+        "last_name": "Esposito",
+        "first_name": "Phil",
+        "full_name":"Phil Esposito",
+        "stat": "1590"
+    }]
+}]
+
+
+module.exports = {
+  mostPoints: mostPoints
+}
