a9f6c88549196fa7d8da15a866d5ee983e830ca0
blakeparkinson <blake@edmodo.com>
2015-06-09 18:00:32 -0700

    uuuufffffffffffffff

--- routes/quiz.js
+++ routes/quiz.js
@@ -172,7 +172,9 @@ var fetchQuizScores = function(req, team_id){
             mod_scores.push(items[i].modified_score);
           }
         }
-      }
+      
+}
+console.log(req);
     req.session.scores = {};
     req.session.scores.all_scores = mod_scores;
     // Assign each quiz score to a bucket for graph display
