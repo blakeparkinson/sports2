c1a82fc7afd734e1a448f283b7dc7a048e190f80
HenryEdmodo <henry@edmodo.com>
2015-06-10 18:36:03 -0700

    mlb, most home runs
    most home runs

---
+++ lists/mlb/mostHomeRuns.js
@@ -0,0 +1,94 @@
+var mostHomeRuns = [
+  {
+  "league": "mlb",
+  "type":"goats",
+  "keywords": "mlb home runs hits baseball",
+  "category": "mostHomeruns",
+  "description": "Most Home Runs in MLB History",
+  "players": [
+    {
+        "player_id": "1",
+        "rank": "1",
+        "last_name": "Bonds",
+        "first_name": "Barry",
+        "full_name":"Barry Bonds",
+        "stat": "762"
+    },
+    {
+        "player_id": "2",
+        "rank": "2",
+        "last_name": "Aaron",
+        "first_name": "Hank",
+        "full_name":"Hank Aaron",
+        "stat": "755"
+    },
+    {
+        "player_id": "3",
+        "rank": "3",
+        "last_name": "Ruth",
+        "first_name": "Babe",
+        "full_name":"Babe Ruth",
+        "stat": "714"
+    },
+    {
+        "player_id": "4",
+        "rank": "4",
+        "last_name": "Rodriguez",
+        "first_name": "Alex",
+        "full_name":"Alex Rodriguez",
+        "stat": "665"
+    },
+    {
+        "player_id": "5",
+        "rank": "5",
+        "last_name": "Mays",
+        "first_name": "Willie",
+        "full_name":"Willie Mays",
+        "stat": "660"
+    },
+    {
+        "player_id": "6",
+        "rank": "6",
+        "last_name": "Griffey Jr.",
+        "first_name": "Ken",
+        "full_name":"Ken Griffey Jr.",
+        "stat": "630"
+    },
+    {
+        "player_id": "7",
+        "rank": "7",
+        "last_name": "Thome",
+        "first_name": "Jim",
+        "full_name":"Jim Thome",
+        "stat": "612"
+    },
+    {
+        "player_id": "8",
+        "rank": "8",
+        "last_name": "Sosa",
+        "first_name": "Sammy",
+        "full_name":"Sammy Sosa",
+        "stat": "609"
+    },
+    {
+        "player_id": "9",
+        "rank": "9",
+        "last_name": "Robinson",
+        "first_name": "Frank",
+        "full_name":"Frank Robinson",
+        "stat": "586"
+    },
+    {
+        "player_id": "10",
+        "rank": "10",
+        "last_name": "McGwire",
+        "first_name": "Mark",
+        "full_name":"Mark McGwire",
+        "stat": "583"
+    }]
+}]
+
+
+module.exports = {
+  mostHomeRuns: mostHomeRuns
+}
