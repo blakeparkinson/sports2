da75e0b9326a6298d64b777ad47578785f284f1a
HenryEdmodo <henry@edmodo.com>
2015-06-10 21:18:29 -0700

    this is WOAT
    need new system

---
+++ lists/nfl/mostPassingTD.js
@@ -0,0 +1,94 @@
+var mostPassingTD = [
+  {
+  "league": "nfl",
+  "type":"goats",
+  "keywords": "nfl football most passing TDs quarterback",
+  "category": "mostPassingTD",
+  "description": "Most Passing Touchdowns in NFL History",
+  "players": [
+    {
+        "player_id": "1",
+        "rank": "1",
+        "last_name": "Manning",
+        "first_name": "Peyton",
+        "full_name":"Peyton Manning",
+        "stat": "530"
+    },
+    {
+        "player_id": "2",
+        "rank": "2",
+        "last_name": "Favre",
+        "first_name": "Brett",
+        "full_name":"Brett Favre",
+        "stat": "508"
+    },
+    {
+        "player_id": "3",
+        "rank": "3",
+        "last_name": "Marino",
+        "first_name": "Dan",
+        "full_name":"Dan Marino",
+        "stat": "420"
+    },
+    {
+        "player_id": "4",
+        "rank": "4",
+        "last_name": "Brees",
+        "first_name": "Drew",
+        "full_name":"Drew Brees",
+        "stat": "396 "
+    },
+    {
+        "player_id": "5",
+        "rank": "5",
+        "last_name": "Brady",
+        "first_name": "Tom",
+        "full_name":"Tom Brady",
+        "stat": "392"
+    },
+    {
+        "player_id": "6",
+        "rank": "6",
+        "last_name": "Tarkenton",
+        "first_name": "Fran",
+        "full_name":"Fran Tarkenton",
+        "stat": "342"
+    },
+    {
+        "player_id": "7",
+        "rank": "7",
+        "last_name": "Elway",
+        "first_name": "John",
+        "full_name":"John Elway",
+        "stat": "300"
+    },
+    {
+        "player_id": "8",
+        "rank": "8",
+        "last_name": "Moon",
+        "first_name": "Warren",
+        "full_name":"Warren Moon",
+        "stat": "291"
+    },
+    {
+        "player_id": "9",
+        "rank": "9",
+        "last_name": "Unitas",
+        "first_name": "Johnny",
+        "full_name": "Johnny Unitas",
+        "stat": "290"
+    },
+    {
+        "player_id": "10",
+        "rank": "10",
+        "last_name": "Testaverde",
+        "first_name": "Vinny",
+        "full_name":"Vinny Testaverde",
+        "stat": "275"
+    }]
+}]
+
+
+module.exports = {
+  mostPassingTD: mostPassingTD
+}
---
+++ lists/nfl/mostRushingYards.js
@@ -0,0 +1,94 @@
+var mostRushingYards = [
+  {
+  "league": "nfl",
+  "type":"goats",
+  "keywords": "nfl football most rushing yards running back",
+  "category": "mostRushingYards",
+  "description": "Most Rushing Yards in NFL History",
+  "players": [
+    {
+        "player_id": "1",
+        "rank": "1",
+        "last_name": "Smith",
+        "first_name": "Emmitt",
+        "full_name":"Emmitt Smith",
+        "stat": "18355"
+    },
+    {
+        "player_id": "2",
+        "rank": "2",
+        "last_name": "Payton",
+        "first_name": "Walter",
+        "full_name":"Walter Payton",
+        "stat": "16726"
+    },
+    {
+        "player_id": "3",
+        "rank": "3",
+        "last_name": "Sanders",
+        "first_name": "Barry",
+        "full_name":"Barry Sanders",
+        "stat": "15269"
+    },
+    {
+        "player_id": "4",
+        "rank": "4",
+        "last_name": "Martin",
+        "first_name": "Curtis",
+        "full_name":"Curtis Martin",
+        "stat": "14101 "
+    },
+    {
+        "player_id": "5",
+        "rank": "5",
+        "last_name": "Tomlinson",
+        "first_name": "LaDainian",
+        "full_name":"LaDainian Tomlinson",
+        "stat": "13684"
+    },
+    {
+        "player_id": "6",
+        "rank": "6",
+        "last_name": "Bettis",
+        "first_name": "Jerome",
+        "full_name":"Jerome Bettis",
+        "stat": "13662"
+    },
+    {
+        "player_id": "7",
+        "rank": "7",
+        "last_name": "Dickerson",
+        "first_name": "Eric",
+        "full_name":"Eric Dickerson",
+        "stat": "13259"
+    },
+    {
+        "player_id": "8",
+        "rank": "8",
+        "last_name": "Dorsett",
+        "first_name": "Tony",
+        "full_name":"Tony Dorsett",
+        "stat": "12739"
+    },
+    {
+        "player_id": "9",
+        "rank": "9",
+        "last_name": "Brown",
+        "first_name": "Jim",
+        "full_name":"Jim Brown",
+        "stat": "12312"
+    },
+    {
+        "player_id": "10",
+        "rank": "10",
+        "last_name": "Faulk",
+        "first_name": "Marshall",
+        "full_name":"Marshall Faulk",
+        "stat": "12279"
+    }]
+}]
+
+
+module.exports = {
+  mostRushingYards: mostRushingYards
+}
