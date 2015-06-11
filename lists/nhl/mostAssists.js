6a0f7c102c992514b8bf2ce123217145a7ef9c32
HenryEdmodo <henry@edmodo.com>
2015-06-10 18:16:02 -0700

    assists list, nhl
    nhl

---
+++ lists/nhl/mostAssists.js
@@ -0,0 +1,94 @@
+var mostAssists = [
+  {
+  "league": "nhl",
+  "type":"goats",
+  "keywords": "nhl assists points hockey",
+  "category": "mostAssists",
+  "description": "Most Assists in NHL History",
+  "players": [
+    {
+        "player_id": "1",
+        "rank": "1",
+        "last_name": "Gretzky",
+        "first_name": "Wayne",
+        "full_name":"Wayne Gretzky",
+        "stat": "1963"
+    },
+    {
+        "player_id": "2",
+        "rank": "2",
+        "last_name": "Francis",
+        "first_name": "Ron",
+        "full_name":"Ron Francis",
+        "stat": "1249"
+    },
+    {
+        "player_id": "3",
+        "rank": "3",
+        "last_name": "Messier",
+        "first_name": "Mark",
+        "full_name":"Mark Messier",
+        "stat": "1193"
+    },
+    {
+        "player_id": "4",
+        "rank": "4",
+        "last_name": "Bourque",
+        "first_name": "Ray",
+        "full_name":"Ray Bourque",
+        "stat": "1169"
+    },
+    {
+        "player_id": "5",
+        "rank": "5",
+        "last_name": "Coffey",
+        "first_name": "Paul",
+        "full_name":"Paul Coffey",
+        "stat": "1135"
+    },
+    {
+        "player_id": "6",
+        "rank": "6",
+        "last_name": "Jagr",
+        "first_name": "Jaromir",
+        "full_name":"Jaromir Jagr",
+        "stat": "1080"
+    },
+    {
+        "player_id": "7",
+        "rank": "7",
+        "last_name": "Oates",
+        "first_name": "Adam",
+        "full_name":"Adam Oates",
+        "stat": "1079"
+    },
+    {
+        "player_id": "8",
+        "rank": "8",
+        "last_name": "Yzerman",
+        "first_name": "Steve",
+        "full_name":"Steve Yzerman",
+        "stat": "1063"
+    },
+    {
+        "player_id": "9",
+        "rank": "9",
+        "last_name": "Howe",
+        "first_name": "Gordie",
+        "full_name":"Gordie Howe",
+        "stat": "1049"
+    },
+    {
+        "player_id": "10",
+        "rank": "10",
+        "last_name": "Dionne",
+        "first_name": "Marcel",
+        "full_name":"Marcel Dionne",
+        "stat": "1040"
+    }]
+}]
+
+
+module.exports = {
+  mostAssists: mostAssists
+}
