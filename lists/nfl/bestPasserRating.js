d544d2cb9b24e07c1618915577be46efed380f87
HenryEdmodo <henry@edmodo.com>
2015-06-12 15:35:00 -0700

    more CW
    - nfl: passer rating, passing yards, passing TDs,
    - mlb: stolen

---
+++ lists/nfl/bestPasserRating.js
@@ -0,0 +1,94 @@
+var bestPasserRating = [
+  {
+  "league": "nfl",
+  "type":"goats",
+  "keywords": "nfl football best passer rating passing quarterback",
+  "category": "bestPasserRating",
+  "description": "Best Passer Ratings in NFL History",
+  "players": [
+    {
+        "player_id": "1",
+        "rank": "1",
+        "last_name": "Rodgers",
+        "first_name": "Aaron",
+        "full_name":"Aaron Rodgers",
+        "stat": "106.0"
+    },
+    {
+        "player_id": "2",
+        "rank": "2",
+        "last_name": "Romo",
+        "first_name": "Tony",
+        "full_name":"Tony Romo",
+        "stat": "97.6"
+    },
+    {
+        "player_id": "3",
+        "rank": "3",
+        "last_name": "Manning",
+        "first_name": "Peyton",
+        "full_name":"Peyton Manning",
+        "stat": "97.5 "
+    },
+    {
+        "player_id": "4",
+        "rank": "4",
+        "last_name": "Young",
+        "first_name": "Steve",
+        "full_name":"Steve Young",
+        "stat": "96.8"
+    },
+    {
+        "player_id": "5",
+        "rank": "5",
+        "last_name": "Brady",
+        "first_name": "Tom",
+        "full_name":"Tom Brady",
+        "stat": "95.9"
+    },
+    {
+        "player_id": "6",
+        "rank": "6",
+        "last_name": "Rivers",
+        "first_name": "Philip",
+        "full_name":"Philip Rivers",
+        "stat": "95.7 "
+    },
+    {
+        "player_id": "7",
+        "rank": "7",
+        "last_name": "Brees",
+        "first_name": "Drew",
+        "full_name":"Drew Brees",
+        "stat": "95.4"
+    },
+    {
+        "player_id": "8",
+        "rank": "8",
+        "last_name": "Roethlisberger",
+        "first_name": "Ben",
+        "full_name":"Ben Roethlisberger",
+        "stat": "93.9"
+    },
+    {
+        "player_id": "9",
+        "rank": "9",
+        "last_name": "Warner",
+        "first_name": "Kurt",
+        "full_name": "Kurt Warner",
+        "stat": "93.7"
+    },
+    {
+        "player_id": "10",
+        "rank": "10",
+        "last_name": "Montana",
+        "first_name": "Joe",
+        "full_name":"Joe Montana",
+        "stat": "92.3"
+    }]
+}]
+
+
+module.exports = {
+  bestPasserRating: bestPasserRating
+}
