618e72128e8b525bb481666f4918bf953835ca20
HenryEdmodo <henry@edmodo.com>
2015-06-13 10:03:24 -0700

    call me charlie
    - more lists
    - add “warming up” section to leaguesearch page

---
+++ lists/mlb/mostStolenBases.js
@@ -0,0 +1,94 @@
+var mostStolenBases = [
+  {
+  "league": "mlb",
+  "type":"goats",
+  "keywords": "mlb stolen bases stealing baseball",
+  "category": "mostStolenBases",
+  "description": "Most Stolen Bases in MLB History",
+  "players": [
+    {
+        "player_id": "1",
+        "rank": "1",
+        "last_name": "Henderson",
+        "first_name": "Rickey",
+        "full_name":"Rickey Henderson",
+        "stat": "1406"
+    },
+    {
+        "player_id": "2",
+        "rank": "2",
+        "last_name": "Brock",
+        "first_name": "Lou",
+        "full_name":"Lou Brock",
+        "stat": "938"
+    },
+    {
+        "player_id": "3",
+        "rank": "3",
+        "last_name": "Hamilton",
+        "first_name": "Billy",
+        "full_name":"Billy Hamilton",
+        "stat": "914"
+    },
+    {
+        "player_id": "4",
+        "rank": "4",
+        "last_name": "Cobb",
+        "first_name": "Ty",
+        "full_name":"Ty Cobb",
+        "stat": "897"
+    },
+    {
+        "player_id": "5",
+        "rank": "5",
+        "last_name": "Raines",
+        "first_name": "Tim",
+        "full_name":"Tim Raines",
+        "stat": "808"
+    },
+    {
+        "player_id": "6",
+        "rank": "6",
+        "last_name": "Coleman",
+        "first_name": "Vince",
+        "full_name":"Vince Coleman",
+        "stat": "752"
+    },
+    {
+        "player_id": "7",
+        "rank": "7",
+        "last_name": "Latham",
+        "first_name": "Arlie",
+        "full_name":"Arlie Latham",
+        "stat": "742"
+    },
+    {
+        "player_id": "8",
+        "rank": "8",
+        "last_name": "Collins",
+        "first_name": "Eddie",
+        "full_name":"Eddie Collins",
+        "stat": "741"
+    },
+    {
+        "player_id": "9",
+        "rank": "9",
+        "last_name": "Carey",
+        "first_name": "Max",
+        "full_name":"Max Carey",
+        "stat": "738"
+    },
+    {
+        "player_id": "10",
+        "rank": "10",
+        "last_name": "Wagner",
+        "first_name": "Honus",
+        "full_name":"Honus Wagner",
+        "stat": "723"
+    }]
+}]
+
+
+module.exports = {
+  mostStolenBases: mostStolenBases
+}
