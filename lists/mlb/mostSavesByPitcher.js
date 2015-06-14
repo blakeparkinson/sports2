76747a2ebf2b83a0e2294745ea331cffbab0bd87
HenryEdmodo <henry@edmodo.com>
2015-06-14 15:06:33 -0700

    a few good lists
    - saves by pitcher
    - shutouts by pitcher
    - strikeouts by pitcher

---
+++ lists/mlb/mostSavesByPitcher.js
@@ -0,0 +1,94 @@
+var mostSavesByPitcher = [
+  {
+  "league": "mlb",
+  "type":"goats",
+  "keywords": "mlb most saves closer close pitcher pitching perfect",
+  "category": "mostSavesByPitcher",
+  "description": "Most Saves by Pitcher in MLB History",
+  "players": [
+    {
+        "player_id": "1",
+        "rank": "1",
+        "last_name": "Rivera",
+        "first_name": "Mariano",
+        "full_name":"Mariano Rivera",
+        "stat": "652"
+    },
+    {
+        "player_id": "2",
+        "rank": "2",
+        "last_name": "Hoffman",
+        "first_name": "Trevor",
+        "full_name":"Trevor Hoffman",
+        "stat": "601"
+    },
+    {
+        "player_id": "3",
+        "rank": "3",
+        "last_name": "Smith",
+        "first_name": "Lee",
+        "full_name":"Lee Smith",
+        "stat": "478"
+    },
+    {
+        "player_id": "4",
+        "rank": "4",
+        "last_name": "Franco",
+        "first_name": "John",
+        "full_name":"John Franco",
+        "stat": "424"
+    },
+    {
+        "player_id": "5",
+        "rank": "5",
+        "last_name": "Wagner",
+        "first_name": "Billy",
+        "full_name":"Billy Wagner",
+        "stat": "422"
+    },
+    {
+        "player_id": "6",
+        "rank": "6",
+        "last_name": "Eckersley",
+        "first_name": "Dennis",
+        "full_name":"Dennis Eckersley",
+        "stat": "390"
+    },
+    {
+        "player_id": "7",
+        "rank": "7",
+        "last_name": "Nathan",
+        "first_name": "Joe",
+        "full_name":"Joe Nathan",
+        "stat": "377"
+    },
+    {
+        "player_id": "8",
+        "rank": "8",
+        "last_name": "Reardon",
+        "first_name": "Jeff",
+        "full_name":"Jeff Reardon",
+        "stat": "367"
+    },
+    {
+        "player_id": "9",
+        "rank": "9",
+        "last_name": "Rodriguez",
+        "first_name": "Francisco",
+        "full_name":"Francisco Rodriguez",
+        "stat": "361"
+    },
+    {
+        "player_id": "10",
+        "rank": "10",
+        "last_name": "Percival",
+        "first_name": "Troy",
+        "full_name":"Troy Percival",
+        "stat": "358"
+    }]
+}]
+
+
+module.exports = {
+  mostSavesByPitcher: mostSavesByPitcher
+}
