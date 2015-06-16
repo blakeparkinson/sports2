dbd8efff1ef2deba6e7d1163423cdd68699887cb
HenryEdmodo <henry@edmodo.com>
2015-06-15 21:02:39 -0700

    charlie work
    nhl lists

---
+++ lists/nhl/highestSavePercentage.js
@@ -0,0 +1,95 @@
+var highestSavePercentage = [
+  {
+  "league": "nhl",
+  "type":"goats",
+  "keywords": "nhl save shutout goalie keeper hockey",
+  "category": "highestSavePercentage",
+  "description": "Highest Save Percentage in NHL History",
+  "players": [
+    {
+        "player_id": "1",
+        "rank": "1",
+        "last_name": "Rask",
+        "first_name": "Tuukka",
+        "full_name":"Tuukka Rask",
+        "stat": "0.926"
+    },
+    {
+        "player_id": "2",
+        "rank": "2",
+        "last_name": "Schneider",
+        "first_name": "Cory",
+        "full_name":"Cory Schneider",
+        "stat": "0.925"
+    },
+    {
+        "player_id": "3",
+        "rank": "3",
+        "last_name": "Hašek",
+        "first_name": "Dominik",
+        "full_name":"Dominik Hašek",
+        "stat": "0.922"
+    },
+    {
+        "player_id": "4",
+        "rank": "4",
+        "last_name": "Lundqvist",
+        "first_name": "Henrik",
+        "full_name":"Henrik Lundqvist",
+        "stat": "0.921"
+    },
+    {
+        "player_id": "5",
+        "rank": "5",
+        "last_name": "Thomas",
+        "first_name": "Tim",
+        "full_name":"Tim Thomas",
+        "stat": "0.920"
+    },
+    {
+        "player_id": "6",
+        "rank": "6",
+        "last_name": "Price",
+        "first_name": "Carey",
+        "full_name":"Carey Price",
+        "stat": "81"
+        "stat": "0.919"
+    },
+    {
+        "player_id": "7",
+        "rank": "7",
+        "last_name": "Luongo",
+        "first_name": "Roberto",
+        "full_name":"Roberto Luongo",
+        "stat": "0.919"
+    },
+    {
+        "player_id": "8",
+        "rank": "8",
+        "last_name": "Rinne",
+        "first_name": "Pekka",
+        "full_name":"Pekka Rinne",
+        "stat": "0.919"
+    },
+    {
+        "player_id": "9",
+        "rank": "9",
+        "last_name": "Cechmánek",
+        "first_name": "Roman",
+        "full_name":"Roman Cechmánek",
+        "stat": "0.919"
+    },
+    {
+        "player_id": "10",
+        "rank": "10",
+        "last_name": "Bobrovsky",
+        "first_name": "Sergei",
+        "full_name":"Sergei Bobrovsky",
+        "stat": "0.919"
+    }]
+}]
+
+
+module.exports = {
+  highestSavePercentage: highestSavePercentage
+}
