0533fb563b4ffa61d7d99e3a4eecc6672041cdae
HenryEdmodo <henry@edmodo.com>
2015-06-16 08:57:47 -0700

    warming up lists
    - nfl
    - mlb

---
+++ lists/mlb/warmingUp/nameMlbTeams.js
@@ -0,0 +1,196 @@
+var nameMlbTeams = [
+{
+  "league": "mlb",
+  "type": "trivia",
+  "bucket": "team"
+  "category": "nameMlbTeams",
+  "keywords": "name all MLB teams franchises",
+  "description": "Name All MLB Teams",
+  "teams": [
+  {
+    "rank":"1",
+    "name": "Braves",
+    "market":"Atlanta",
+    "year":"2010"
+  },
+  {
+    "rank":"2",
+    "name":"Marlins",
+    "market":"Florida",
+    "year":"2012",
+  },
+  {
+    "rank":"3",
+    "name":"Mets",
+    "market":"New York",
+    "year":"2012",
+  },
+  {
+    "rank":"4",
+    "name":"Phillies",
+    "market":"Philadelphia",
+    "year":"2012",
+  },
+  {
+    "rank":"5",
+    "name":"Nationals",
+    "market":"Washington",
+    "year":"2012",
+  },
+  {
+    "rank":"6",
+    "name":"Cubs",
+    "market":"Chicago",
+    "year":"2012",
+  },
+  {
+    "rank":"7",
+    "name":"Reds",
+    "market":"Cincinnati",
+    "year":"2012",
+  },
+  {
+    "rank":"8",
+    "name":"Astros",
+    "market":"Houston",
+    "year":"2012",
+  },
+  {
+    "rank":"9",
+    "name":"Brewers",
+    "market":"Milwaukee",
+    "year":"2012",
+  },
+  {
+    "rank":"10",
+    "name":"Pirates",
+    "market":"Pittsburgh",
+    "year":"2012",
+  },
+  {
+    "rank":"11",
+    "name": "Cardinals",
+    "market":"St. Louis",
+    "year":"2010"
+  },
+  {
+    "rank":"12",
+    "name":"Diamondbacks",
+    "market":"Arizona",
+    "year":"2012",
+  },
+  {
+    "rank":"13",
+    "name":"Rockies",
+    "market":"Colorado",
+    "year":"2012",
+  },
+  {
+    "rank":"14",
+    "name":"Dodgers",
+    "market":"Los Angeles",
+    "year":"2012",
+  },
+  {
+    "rank":"15",
+    "name":"Padres",
+    "market":"San Diego",
+    "year":"2012",
+  },
+  {
+    "rank":"16",
+    "name":"Giants",
+    "market":"San Francisco",
+    "year":"2012",
+  },
+  {
+    "rank":"17",
+    "name":"Orioles",
+    "market":"Baltimore",
+    "year":"2012",
+  },
+  {
+    "rank":"18",
+    "name":"Red Sox",
+    "market":"Boston",
+    "year":"2012",
+  },
+  {
+    "rank":"19",
+    "name":"Yankees",
+    "market":"New York",
+    "year":"2012",
+  },
+  {
+    "rank":"20",
+    "name":"Rays",
+    "market":"Tampa Bay",
+    "year":"2012",
+  },
+  {
+    "rank":"21",
+    "name": "Blue Jays",
+    "market":"Toronto",
+    "year":"2010"
+  },
+  {
+    "rank":"22",
+    "name":"White Sox",
+    "market":"Chicago",
+    "year":"2012",
+  },
+  {
+    "rank":"23",
+    "name":"Indians",
+    "market":"Cleveland",
+    "year":"2012",
+  },
+  {
+    "rank":"24",
+    "name":"Tigers",
+    "market":"Detroit",
+    "year":"2012",
+  },
+  {
+    "rank":"25",
+    "name":"Royals",
+    "market":"Kansas City",
+    "year":"2012",
+  },
+  {
+    "rank":"26",
+    "name":"Twins",
+    "market":"Minnesota",
+    "year":"2012",
+  },
+  {
+    "rank":"27",
+    "name":"Angels",
+    "market":"Los Angeles",
+    "year":"2012",
+  },
+  {
+    "rank":"28",
+    "name":"Athletics",
+    "market":"Oakland",
+    "year":"2012",
+  },
+  {
+    "rank":"29",
+    "name":"Mariners",
+    "market":"Seattle",
+    "year":"2012",
+  },
+  {
+    "rank":"30",
+    "name":"Rangers",
+    "market":"Texas",
+    "year":"2012",
+  },
+  
+  ]
+}]
+
+module.exports = {
+  nameMlbTeams: nameMlbTeams
+}
---
+++ lists/nfl/warmingUp/nameNflTeams.js
@@ -0,0 +1,207 @@
+var nameNflTeams = [
+{
+  "league": "nfl",
+  "type": "trivia",
+  "bucket": "team"
+  "category": "nameNflTeams",
+  "keywords": "name all NFL teams franchises",
+  "description": "Name All NFL Teams",
+  "teams": [
+  {
+    "rank":"1",
+    "name": "Bills",
+    "market":"Buffalo",
+    "year":"2010"
+  },
+  {
+    "rank":"2",
+    "name":"Dolphins",
+    "market":"Miami",
+    "year":"2012",
+  },
+  {
+    "rank":"3",
+    "name":"Patriots",
+    "market":"New England",
+    "year":"2012",
+  },
+  {
+    "rank":"4",
+    "name":"Jets",
+    "market":"New York",
+    "year":"2012",
+  },
+  {
+    "rank":"5",
+    "name":"Ravens",
+    "market":"Baltimore",
+    "year":"2012",
+  },
+  {
+    "rank":"6",
+    "name":"Bengals",
+    "market":"Cincinnati",
+    "year":"2012",
+  },
+  {
+    "rank":"7",
+    "name":"Browns",
+    "market":"Cleveland",
+    "year":"2012",
+  },
+  {
+    "rank":"8",
+    "name":"Steelers",
+    "market":"Pittsburgh",
+    "year":"2012",
+  },
+  {
+    "rank":"9",
+    "name":"Texans",
+    "market":"Houston",
+    "year":"2012",
+  },
+  {
+    "rank":"10",
+    "name":"Colts",
+    "market":"Indianapolis",
+    "year":"2012",
+  },
+  {
+    "rank":"11",
+    "name": "Jaguars",
+    "market":"Jacksonville",
+    "year":"2010"
+  },
+  {
+    "rank":"12",
+    "name":"Titans",
+    "market":"Tennessee",
+    "year":"2012",
+  },
+  {
+    "rank":"13",
+    "name":"Broncos",
+    "market":"Denver",
+    "year":"2012",
+  },
+  {
+    "rank":"14",
+    "name":"Chiefs",
+    "market":"Kansas City",
+    "year":"2012",
+  },
+  {
+    "rank":"15",
+    "name":"Raiders",
+    "market":"Oakland",
+    "year":"2012",
+  },
+  {
+    "rank":"16",
+    "name":"Chargers",
+    "market":"San Diego",
+    "year":"2012",
+  },
+  {
+    "rank":"17",
+    "name":"Cowboys",
+    "market":"Dallas",
+    "year":"2012",
+  },
+  {
+    "rank":"18",
+    "name":"Giants",
+    "market":"New York",
+    "year":"2012",
+  },
+  {
+    "rank":"19",
+    "name":"Eagles",
+    "market":"Philadelphia",
+    "year":"2012",
+  },
+  {
+    "rank":"20",
+    "name":"Redskins",
+    "market":"Washington",
+    "year":"2012",
+  },
+  {
+    "rank":"21",
+    "name": "Bears",
+    "market":"Chicago",
+    "year":"2010"
+  },
+  {
+    "rank":"22",
+    "name":"Lions",
+    "market":"Detroit",
+    "year":"2012",
+  },
+  {
+    "rank":"23",
+    "name":"Packers",
+    "market":"Green Bay",
+    "year":"2012",
+  },
+  {
+    "rank":"24",
+    "name":"Vikings",
+    "market":"Minnesota",
+    "year":"2012",
+  },
+  {
+    "rank":"25",
+    "name":"Falcons",
+    "market":"Atlanta",
+    "year":"2012",
+  },
+  {
+    "rank":"26",
+    "name":"Panthers",
+    "market":"Carolina",
+    "year":"2012",
+  },
+  {
+    "rank":"27",
+    "name":"Saints",
+    "market":"New Orleans",
+    "year":"2012",
+  },
+  {
+    "rank":"28",
+    "name":"Buccaneers",
+    "market":"Tampa Bay",
+    "year":"2012",
+  },
+  {
+    "rank":"29",
+    "name":"Cardinals",
+    "market":"Arizona",
+    "year":"2012",
+  },
+  {
+    "rank":"30",
+    "name":"Rams",
+    "market":"St. Louis",
+    "year":"2012",
+  },
+  {
+    "rank":"31",
+    "name":"49ers",
+    "market":"San Francisco",
+    "year":"2012",
+  },
+  {
+    "rank":"32",
+    "name":"Seahawks",
+    "market":"Seattle",
+    "year":"2012",
+  }
+  ]
+}]
+
+module.exports = {
+  nameNflTeams: nameNflTeams
+}
